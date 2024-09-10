67c774f9967d6a22698e95940915652004c74f4a
YashGames2007 <yashbhavsar.dev2007@gmail.com>
2024-09-10 10:58:41 +0530

    V0.2.7.7 - Added Backend Sources
    Adding some Backend Specializations.
    
    TIP: Commit not fully prepared still under progress
    
    Further Version Improvements
      - Add Comments
      - Rename Variables Correctly
      - Refactor all Files with Code Formatting
      - Recheck the Functions to scope for Efficiency Improvement & Complexity Reduction

--- app/src/main/java/com/shubhamdeshmukh/attendencemanagementsystem/backend/database_entities/Monitor.java
+++ app/src/main/java/com/shubhamdeshmukh/attendencemanagementsystem/backend/database_entities/Monitor.java
@@ -4,27 +4,27 @@ import com.shubhamdeshmukh.attendencemanagementsystem.backend.other_entities.Acc
 
 public class Monitor extends Account {
 
-    private String enrollmentNo;
+    private String id;
     private String className;
 
     public Monitor() { super("Monitor"); }
 
     public Monitor(String name, String enNo, String userId) {
         super(name, userId, "Monitor");
-        this.enrollmentNo = enNo;
+        this.id = enNo;
     }
 
     public Monitor(String name, String enNo, String userId, String className) {
         super(name, userId, "Monitor");
         this.className = className;
-        this.enrollmentNo = enNo;
+        this.id = enNo;
     }
 
     public String getClassName() {
         return className;
     }
 
-    public String getEnrollmentNo() { return enrollmentNo; }
+    public String getId() { return id; }
 
-    public String printInfo() { return "Name: " + getName() + "En.No: " + enrollmentNo + " Class: " + className; }
+    public String printInfo() { return "Name: " + getName() + "En.No: " + id + " Class: " + className; }
 }
